1c22545662bfff31036deeffc17f3c2311ae73f5
Amit Prajapati <amit.prajapati@stl.tech>
2021-11-02 11:38:19 +0530

    new

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -5,6 +5,7 @@ public class Test2 {
 		// TODO Auto-generated method stub
 		System.out.println("my first github code new");
 		System.out.println("From Ubantu machine");
+		System.out.println("From Fedora machine");
 
 	}
 
